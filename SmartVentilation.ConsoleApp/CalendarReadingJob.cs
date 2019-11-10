988f14e9d00e6fb0f447bacc2ffe3562dab3d8ab
Ondrej Stieber <ondrej.stieber@tsium.com>
2019-11-10 17:20:18 +0100

    Čtení událostí z Google kalendáře.

---
+++ SmartVentilation.ConsoleApp/CalendarReadingJob.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Quartz;
+
+namespace SmartVentilation.ConsoleApp
+{
+    [DisallowConcurrentExecution]
+    public class CalendarReadingJob : IJob
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/calendar-dotnet-quickstart.json
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Smart Ventilation";
+
+        /// <summary>
+        /// Spuštění výkonné části jobu
+        /// </summary>
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"{DateTime.Now} - Calendar read");
+            ReadFutureCalendarItems();
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Čtení událostí z kalendáře
+        /// https://developers.google.com/calendar/quickstart/dotnet
+        /// </summary>
+        public void ReadFutureCalendarItems()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("nek71fi8o6nrkukvusvdbv90i8@group.calendar.google.com");
+            request.TimeMin = DateTime.Now;
+            request.TimeMax = DateTime.Now.AddDays(7);
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items != null && events.Items.Count > 0)
+            {
+                foreach (var eventItem in events.Items)
+                {
+                    string when = eventItem.Start.DateTime.ToString();
+                    if (String.IsNullOrEmpty(when))
+                    {
+                        when = eventItem.Start.Date;
+                    }
+                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No upcoming events found.");
+            }
+            Console.Read();
+
+        }
+    }
+}
--- SmartVentilation.ConsoleApp/Program.cs
+++ SmartVentilation.ConsoleApp/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Triggers;
 
 namespace SmartVentilation.ConsoleApp
 {
@@ -6,7 +14,31 @@ namespace SmartVentilation.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Job.InitializeJobs();
+
+            Console.WriteLine("Tasks started!");
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Job
+    {
+        public static async void InitializeJobs()
+        {
+            var scheduler = await new StdSchedulerFactory().GetScheduler();
+            await scheduler.Start();
+
+            var userEmailsJob = JobBuilder.Create<CalendarReadingJob>()
+                .WithIdentity("CalendarReadingJob")
+                .Build();
+            var userEmailsTrigger = TriggerBuilder.Create()
+                .WithIdentity("CalendarReadingJobCron")
+                .StartNow()
+                .WithCronSchedule("* * * ? * *")
+                .Build();
+
+            var result = await scheduler.ScheduleJob(userEmailsJob, userEmailsTrigger);
         }
     }
 }
